4d5416a5aacdc6ef92bb0fe98b515531bd2bcc73
Alexander Inman <ainman@ezmetrology.com>
2018-08-10 08:39:04 -0400

    First commit

---
+++ Lab1Pt1_redo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Pt1_redo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            Console.WriteLine("Welcome to the Number Comparison Machine.");
+
+            x = Console.Read();
+            Console.WriteLine(x);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
